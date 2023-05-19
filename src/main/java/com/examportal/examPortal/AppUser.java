d3cd248665ac5e642e198c21f784267167158c1a
unknown <sahootusar642@gmail.com>
2023-05-19 09:48:04 -0700

    added id to app user

--- src/main/java/com/examportal/examPortal/AppUser.java
+++ src/main/java/com/examportal/examPortal/AppUser.java
@@ -1,4 +1,18 @@
 package com.examportal.examPortal;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class AppUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
 }
