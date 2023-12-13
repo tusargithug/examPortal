cf53f84625be4224908bd06140e9a27595a24f92
Tusar Sahoo <tusar.sahoo@thrymr.net>
2023-12-13 14:17:34 +0530

    static data added in main class

--- src/main/java/com/examportal/examPortal/ExamPortalApplication.java
+++ src/main/java/com/examportal/examPortal/ExamPortalApplication.java
@@ -1,6 +1,11 @@
 package com.examportal.examPortal;
 
 
+import com.examportal.examPortal.Enum.Role;
+import com.examportal.examPortal.Model.AppUser;
+import com.examportal.examPortal.Repository.AppUserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +13,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExamPortalApplication {
-
+        //implements CommandLineRunner
+@Autowired
+private AppUserRepo userRepo;
     public static void main(String[] args) {
         SpringApplication.run(ExamPortalApplication.class, args);
         System.out.println("===========================================");
@@ -19,4 +26,15 @@ public class ExamPortalApplication {
 
 
     }
+//    @Override
+//    public void run(String... args) {
+//        appUser();
+//    }
+//
+//    public void appUser(){
+//        AppUser user =  new AppUser();
+//        user.setUserName("Abhisek");
+//        user.setRoleType(Role.STUDENT);
+//        userRepo.save(user);
+//    }
 }
